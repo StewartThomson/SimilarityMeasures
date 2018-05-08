d742c15d1a8859bd0d0e1b89fc7c71027380863a
Stewart Thomson <stewart@blackwidow.local>
2018-05-08 16:08:58 -0400

    More testing

--- TestSimilarityMeasures.cs
+++ TestSimilarityMeasures.cs
@@ -29,12 +29,14 @@ namespace SimilarityMeasures
             Console.WriteLine("\nTESTING Frechet:\n" + SimilarityMeasures.Frechet(path1, path2));
             Console.WriteLine("\nTESTING Frechcet with leash length 5:\n" + SimilarityMeasures.Frechet(path1, path2, 5));
 
-            double[,] arr1 = {{0,1,6.9,15.8},{0,1,1.28,1.1}};
+            double[,] arr1 = {{0,1.6,17.85,3.25,27.3},{0,19.2,6.8,14.44,12.6}};
             path1 = Matrix.Build.DenseOfArray(arr1).Transpose();
-            double[,] arr2 = {{0,0.68,6.14,12.73},{4,7.62,11.3,7.76}};
+            double[,] arr2 = {{0,1,2,3,4,5,11.3,40},{0,1,2,3,4,5,6.7,4}};
             path2 = Matrix.Build.DenseOfArray(arr2).Transpose();
             Console.WriteLine("\nTESTING Frechet:\n" + SimilarityMeasures.Frechet(path1, path2));
             Console.WriteLine("\nTESTING Frechcet with leash length 8:\n" + SimilarityMeasures.Frechet(path1, path2, 8));
+            Console.WriteLine("\nTESTING StartEndTranslate:\n" + SimilarityMeasures.StartEndTranslate(path2, path1));
+            Console.WriteLine("\nTESTING LCSS:\n" + SimilarityMeasures.LCSS(path1, path2, 2, 2, 0.5));
         }
     }
 }
